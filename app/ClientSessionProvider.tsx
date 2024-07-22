859bc005dbbc294b108f585bbe9b67f6ac8c61e9
amanjoshi2002 <sctaman21@gmail.com>
2024-07-22 20:51:27 +0530

    till auth

--- app/ClientSessionProvider.tsx
+++ app/ClientSessionProvider.tsx
@@ -2,6 +2,8 @@
 
 import { SessionProvider } from "next-auth/react";
 
+
+
 export default function ClientSessionProvider({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
