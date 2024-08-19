f6d85acfda3c503673655fe8c99ccf5c8c6277d3
amanjoshi2002 <sctaman21@gmail.com>
2024-08-19 14:44:39 +0530

    fixed deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     reactStrictMode: true,
     typescript: {
         ignoreBuildErrors: true,
+    },
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.resolve(__dirname);
+        return config;
     }
 };
 
